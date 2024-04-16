8505b0123103d1ce218ac2827adef201269f0319
nickcyran <nicyran@gmail.com>
2024-04-16 14:09:05 -0400

    server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+// import modules
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// app
+const app = express();
+
+// database
+const mongoString = "PLACEHOLDERATM";
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => console.log(error));
+database.once('connected', () => console.log('Database Connected')); 
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// routes
+const routes = require('./routes');
+app.use("/", routes);
+
+// listener
+const PORT = 9000;
+app.listen(PORT, () => console.log(`Server Started at ${PORT}`))
---
+++ server/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+
+// --ADD ROUTES HERE---
+// EX SMTHING LIKE THIS)
+
+//---USER ROUTES---
+
+//const { getUser, getUsers, getUserByID, postUser } = require('../controllers/userController');
+    //router.get('/getUser', getUser);
+    //router.get('/getUsers', getUsers);
+    //router.get('/getUserByID', getUserByID);
+    //router.post('/createUser', postUser);
+
+// ---PROJECT ROUTES---
+
+//const { getProjects, postProject } = require('../controllers/projectController')
+    //router.get('/getProjects', getProjects);
+    //router.post('/createProject', postProject);
+
