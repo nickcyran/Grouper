2b9d72860818322a4a23281598db13d26d0ab86f
acredick <acredick@albany.edu>
2024-04-19 14:28:30 -0400

    add NavBar + begin calendar

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,5 +2,6 @@ import Messaging from './Messaging';
 import Main from './Main';
 import PageContent from './PageContent'
 import Friends from './Friends'
+import ViewCalendar from './ViewCalendar'
 
-export {Messaging, Main, PageContent, Friends};
+export {Messaging, Main, PageContent, Friends, ViewCalendar};
