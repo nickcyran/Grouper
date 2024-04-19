e43a75bd83fca21771e635d84f62665b21b2f4b8
nickcyran <nicyran@gmail.com>
2024-04-19 14:35:14 -0400

    group

---
+++ client/src/pages/Group.js
@@ -0,0 +1,31 @@
+import { PageContent, Messaging } from '.'
+
+const Left = () => {
+    return (
+        <>
+            channels
+        </>
+    )
+}
+
+const Main = ({ group }) => {
+    return (
+        <>
+            <Messaging group={group} />
+        </>
+    )
+}
+
+const Right = () => {
+    return (
+        <>
+           Members
+        </>
+    )
+}
+
+const Group = ({ group }) => {
+    return <PageContent Left={Left} Main={Main} Right={Right}  group={group} />
+}
+
+export default Group;
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,5 +2,6 @@ import Messaging from './Messaging';
 import Main from './Main';
 import PageContent from './PageContent'
 import Friends from './Friends'
+import Group from './Group'
 
-export {Messaging, Main, PageContent, Friends};
+export {Messaging, Main, PageContent, Friends, Group};
