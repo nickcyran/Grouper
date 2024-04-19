02085b224ac154150434a9f4aa46ede69ee37d26
nickcyran <nicyran@gmail.com>
2024-04-19 14:45:04 -0400

    merged

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,7 +1,8 @@
 import Messaging from './Messaging';
 import Main from './Main';
 import PageContent from './PageContent'
+import Group from './Group'
 import Friends from './Friends'
 import ViewCalendar from './ViewCalendar'
 
-export {Messaging, Main, PageContent, Friends, Group, ViewCalendar};
+export {Messaging, Main, PageContent, Group, Friends, ViewCalendar};
