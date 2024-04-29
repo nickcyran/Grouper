ebd24ac703df8d92623b86d1aa2544f1fde645e0
nickcyran <nicyran@gmail.com>
2024-04-29 14:21:00 -0400

    signout

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -8,19 +8,26 @@ import axios from 'axios';
 export default function Navbar() {
      const navigate = useNavigate();
 
-     const Hand = () => {
+     const signOut = () => {
           localStorage.clear();
           navigate("/Login");
      }
 
      return (<nav className="nav" id="navbar">
-          <Link to="/">Messaging</Link>&nbsp;
-          <Link to="/viewcalendar">Calendar</Link>&nbsp;
-          <Link to="/events">Events</Link>&nbsp;
-          <Link to="/serverhome">Servers</Link>&nbsp;
-          <Link to="/login">Login</Link>&nbsp;
-          <Link to="/signup">Signup</Link>&nbsp;
-          <Link to="/profileSettings">Profile</Link>&nbsp;
-          <div onClick={() => Hand()}>test</div>
+          <div className="wrap">
+               <div className="links">
+                    <Link to="/">Messaging</Link>&nbsp;
+                    <Link to="/viewcalendar">Calendar</Link>&nbsp;
+                    <Link to="/events">Events</Link>&nbsp;
+                    <Link to="/serverhome">Servers</Link>&nbsp;
+                    <Link to="/profileSettings">Profile</Link>&nbsp;
+               </div>
+
+               <div className="signoutBox">
+                    <div className="signoutButton" onClick={() => signOut()}>sign out</div>
+               </div>
+          </div>
+
+
      </nav>)
 }
